448aae917fc0802d46515700a22f65836d3eff14
gem4ik <gema44@yandex.ru>
2022-11-20 22:26:19 +0300

    oop

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,53 +1,54 @@
-let rerenderEntireTree = () => {}
-
-let Data = {
-    Profile: {
-        posts: [
-            { id: 1, post: 'Hello, World', likeValue: 8 },
-            { id: 2, post: "It's my first post", likeValue: 4 },
-            { id: 3, post: "It's my first post", likeValue: 7 }
-        ],
-        newPostText: ''
+let Store = {
+    rerenderEntireTree () {},
+    _Data: {
+        Profile: {
+            posts: [
+                { id: 1, post: 'Hello, World', likeValue: 8 },
+                { id: 2, post: "It's my first post", likeValue: 4 },
+                { id: 3, post: "It's my first post", likeValue: 7 }
+            ],
+            newPostText: ''
+        },
+        Message: {
+            dialogs: [
+                { id: 1, name: 'Sasha' },
+                { id: 2, name: 'Gleb' },
+                { id: 3, name: 'Vlad' },
+                { id: 4, name: 'Misha' },
+                { id: 5, name: 'Kostya' },
+                { id: 6, name: 'Vova' },
+            ],
+            messages: [
+                { id: 1, message: 'I' },
+                { id: 2, message: "don't" },
+                { id: 3, message: 'even' },
+                { id: 4, message: 'know' },
+                { id: 5, message: 'who' },
+                { id: 6, message: 'i' },
+                { id: 7, message: 'am' }
+            ]
+        }
     },
-    Message: {
-        dialogs: [
-            { id: 1, name: 'Sasha' },
-            { id: 2, name: 'Gleb' },
-            { id: 3, name: 'Vlad' },
-            { id: 4, name: 'Misha' },
-            { id: 5, name: 'Kostya' },
-            { id: 6, name: 'Vova' },
-        ],
-        messages: [
-            { id: 1, message: 'I' },
-            { id: 2, message: "don't" },
-            { id: 3, message: 'even' },
-            { id: 4, message: 'know' },
-            { id: 5, message: 'who' },
-            { id: 6, message: 'i' },
-            { id: 7, message: 'am' }
-        ]
+    getData() {
+        return this._Data
+    },
+    addPost () {
+        let newPost = {
+            id: 4,
+            post: Store._Data.Profile.newPostText,
+            likeValue: 0
+        }
+        Store._Data.Profile.posts.push(newPost)
+        Store._Data.Profile.newPostText = ''
+        this.rerenderEntireTree(Store._Data)
+    },
+    updateTextValue (newText) {
+        Store._Data.Profile.newPostText = newText
+        this.rerenderEntireTree(Store._Data)
+    },
+    subject (observer) {
+        this.rerenderEntireTree = observer
     }
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 4,
-        post: Data.Profile.newPostText,
-        likeValue: 0
-    }
-    Data.Profile.posts.push(newPost)
-    Data.Profile.newPostText = ''
-    rerenderEntireTree(Data)
-}
-
-export const updateTextValue = (newText) => {
-    Data.Profile.newPostText = newText
-    rerenderEntireTree(Data)
-}
-
-export const subject = (observer) => {
-    rerenderEntireTree = observer
-}
-
-export default Data
+export default Store
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import './index.css'
-import Data from './components/Data/Data'
-import { addPost, updateTextValue, subject } from './components/Data/Data'
+import Store from './components/Data/Data'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 let rerenderEntireTree = (Data) => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <App Data={Data} addPost={addPost} updateTextValue={updateTextValue} />
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
-  }
-  
-subject(rerenderEntireTree)
+  ReactDOM.render(
+    <React.StrictMode>
+      <App Data={Store.getData()}
+        addPost={Store.addPost.bind(Store)}
+        updateTextValue={Store.updateTextValue.bind(Store)} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+Store.subject(rerenderEntireTree)
 
-rerenderEntireTree(Data)
+rerenderEntireTree(Store.getData())
